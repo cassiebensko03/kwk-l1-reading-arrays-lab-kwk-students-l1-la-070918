69a6e4245c0429a1d368d0caf4b54a0b523e9ceb
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-10 23:28:01 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -23,9 +23,11 @@ end
 first_student_by_method
 
 def last_student_by_method
-#   # Write a solution that returns the first student using the built in .last method
-# end
+  puts STUDENT_NAMES.last
+end
+last_student_by_method
 
-# def first_second_and_third_students
-#   # Write a solution that returns the first, second and third students
-# end
+def first_second_and_third_students
+  puts STUDENT_NAMES.first.second.third
+end
+last_student_by_method
